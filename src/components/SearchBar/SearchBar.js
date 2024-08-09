9e269d04b29d0d59a0240476c2b81adfd9ebe0d6
David Swatzell <thatsstrikethree@gmail.com>
2024-08-09 11:54:14 -0500

    add return to the searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,4 +8,17 @@ function SearchBar({ setArticles }) {
     const data = await response.json();
     setArticles(data.articles);
   };
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={query} 
+        onChange={(e) => setQuery(e.target.value)} 
+        placeholder="Search for news..."
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 }
+
+export default SearchBar;
