b2af3ee5af21e554d9ecb502fcef49dd2f2a8c66
David Swatzell <thatsstrikethree@gmail.com>
2024-08-12 09:54:26 -0500

    add styling to button

--- src/components/BackToResultsButton/BackToResultsButton.js
+++ src/components/BackToResultsButton/BackToResultsButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './BackToResultsButton.css';
 
 const BackToResultsButton = () => {
   const navigate = useNavigate();
